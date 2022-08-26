86ec9a32ac52ae640f7fbbb0fba084053822851f
David Parseen <hello@parseen.dev>
2022-08-26 16:43:05 +0300

    Adds Testnets and verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,36 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.16",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [
+        process.env.HARDHAT_ACCOUNT_0,
+      ],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [
+        process.env.HARDHAT_ACCOUNT_0,
+      ],
+    },
+  },
+  solidity: {
+    version: "0.8.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
