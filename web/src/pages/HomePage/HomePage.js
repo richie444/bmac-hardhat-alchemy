86fa79652def6352e1dbcc4ccbea3c26765c5f5a
David Parseen <hello@parseen.dev>
2022-08-29 11:09:05 +0300

    Updates UI and Adds withdraw functionality

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -14,6 +14,7 @@ const HomePage = () => {
 
   // Component state
   const { currentUser } = useAuth()
+  const [balance, setBalance] = useState(0)
   const [name, setName] = useState('')
   const [message, setMessage] = useState('')
   const [memos, setMemos] = useState([])
@@ -42,17 +43,15 @@ const HomePage = () => {
         const coffeeTxn = await buyMeACoffee.buyCoffee(
           name ? name : 'anon',
           message ? message : 'Enjoy your coffee!',
-          { value: ethers.utils.parseEther('0.001') }
+          { value: ethers.utils.parseEther('0.01') }
         )
+        setName('')
+        setMessage('')
         toast.promise(coffeeTxn.wait(), {
-          loading: 'Buying Coffee..' + '\n' + coffeeTxn.hash,
+          loading: 'Buying Coffee..' + '\n' + `Txn Pending...`,
           success: 'Coffee purchased!' + '\n' + 'Thanks for buying!',
           error: 'Error ' + coffeeTxn.hash + ': ' + coffeeTxn.error,
         })
-
-        // Clear the form fields.
-        setName('')
-        setMessage('')
       }
     } catch (error) {
       toast.error(error)
@@ -84,9 +83,50 @@ const HomePage = () => {
     }
   }
 
+  // Function to withdraw the balance of the contract.
+  const withdraw = async () => {
+    try {
+      const { ethereum } = window
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const buyMeACoffee = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        )
+
+        await buyMeACoffee.withdraw()
+        await getBalance()
+      } else {
+        toast.error('Metamask is not connected')
+      }
+    } catch (error) {
+      toast.error(error)
+    }
+  }
+
+  // Function to fetch the balance of the contract.
+  const getBalance = async () => {
+    try {
+      const { ethereum } = window
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+
+        const balance = await provider.getBalance(contractAddress)
+        setBalance(ethers.utils.formatEther(balance))
+      } else {
+        toast.error('Metamask is not connected')
+      }
+    } catch (error) {
+      toast.error(error)
+    }
+  }
+
   useEffect(() => {
     let buyMeACoffee
     getMemos()
+    getBalance()
 
     // Create an event handler function for when someone sends
     // us a new memo.
@@ -121,62 +161,88 @@ const HomePage = () => {
     }
   }, [])
 
+  useEffect(() => {
+    getBalance()
+  }, [memos])
+
   return (
     <>
       <MetaTags title="Home" />
-      <div className="mx-auto flex max-w-sm items-start space-x-4">
-        <div className="flex-shrink-0">
-          <img
-            className="inline-block h-10 w-10 rounded-full"
-            src={`https://avatars.dicebear.com/api/pixel-art/${currentUser?.address?.toLowerCase()}.svg`}
-            alt=""
-          />
-        </div>
-        <div className="min-w-0 flex-1">
-          <form className="relative">
-            <div className="my-2 overflow-hidden rounded-lg border border-gray-300 shadow-sm">
-              <label htmlFor="name" className="sr-only">
-                Name
-              </label>
-              <div className="mt-1">
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  className="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  placeholder={'Name'}
-                  onChange={onNameChange}
-                />
+      <div className="mx-auto flex max-w-xl items-start space-x-4">
+        {!currentUser && (
+          <div className="min-w-2xl absolute z-10 flex h-[30vh] items-center justify-around bg-gray-400 px-28 opacity-90">
+            <h1 className="text-center text-2xl font-bold">
+              <p className="text-gray-900">🔒 Locked</p>
+              <p className="text-gray-900">Login with Metamask 🦊 to Access</p>
+            </h1>
+          </div>
+        )}
+        <div className="flex w-full justify-between">
+          <div className="my-auto">
+            <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+              <div className="truncate text-sm font-medium text-gray-500">
+                {'Contract Balance'}
+              </div>
+              <div className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+                {balance} ETH
+              </div>
+              <div className="mt-4 cursor-pointer underline" onClick={withdraw}>
+                Withdraw Funds
               </div>
             </div>
-            <div className="overflow-hidden rounded-lg border border-gray-300 shadow-sm">
-              <label htmlFor="comment" className="sr-only">
-                Add your message
-              </label>
-              <textarea
-                rows={3}
-                name="message"
-                id="message"
-                className="block w-full resize-none border-0 p-3 focus:ring-0 sm:text-sm"
-                placeholder="Add your message..."
-                onChange={onMessageChange}
-              />
-            </div>
-          </form>
-          <div className="inset-x-0 bottom-0 flex justify-between py-2">
-            <div className="flex-shrink-0">
-              <button
-                type="submit"
-                className="inline-flex items-center rounded-md border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-                onClick={buyCoffee}
-              >
-                Buy Coffee worth 0.01 ETH
-              </button>
+          </div>
+          <div>
+            <form className="relative w-full">
+              <div className="flex-shrink-0">
+                <img
+                  className="inline-block h-10 w-10 rounded-full"
+                  src={`https://avatars.dicebear.com/api/pixel-art/${currentUser?.address?.toLowerCase()}.svg`}
+                  alt="avatar"
+                />
+              </div>
+              <div className="my-2 overflow-hidden rounded-lg border border-gray-300 shadow-sm">
+                <label htmlFor="name" className="sr-only">
+                  Name
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    placeholder={'Name'}
+                    onChange={onNameChange}
+                  />
+                </div>
+              </div>
+              <div className="overflow-hidden rounded-lg border border-gray-300 shadow-sm">
+                <label htmlFor="comment" className="sr-only">
+                  Add your message
+                </label>
+                <textarea
+                  rows={3}
+                  name="message"
+                  id="message"
+                  className="block w-full resize-none border-0 p-3 focus:ring-0 sm:text-sm"
+                  placeholder="Add your message..."
+                  onChange={onMessageChange}
+                />
+              </div>
+            </form>
+            <div className="inset-x-0 bottom-0 flex justify-between py-2">
+              <div className="flex-shrink-0">
+                <button
+                  type="submit"
+                  className="inline-flex items-center rounded-md border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+                  onClick={buyCoffee}
+                >
+                  Buy Coffee worth 0.01 ETH
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
       {/* Received Memos */}
       <ul role="list" className="divide-y divide-gray-200">
         <h2 className="text-xl font-bold leading-5 text-gray-900 sm:truncate sm:text-2xl sm:tracking-tight">
